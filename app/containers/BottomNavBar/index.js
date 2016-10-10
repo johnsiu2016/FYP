8eb021d1168c34eab766f999930184fc35f1c2ef
johnsiu <buckyscode@gmail.com>
2016-10-11 02:37:46 +0800

    add fix footer css

--- app/containers/BottomNavBar/index.js
+++ app/containers/BottomNavBar/index.js
@@ -5,7 +5,7 @@
  */
 
 import React from 'react';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 
 import FontIcon from 'material-ui/FontIcon';
 import {BottomNavigation, BottomNavigationItem} from 'material-ui/BottomNavigation';
@@ -16,6 +16,8 @@ const recentsIcon = <FontIcon className="material-icons">restore</FontIcon>;
 const favoritesIcon = <FontIcon className="material-icons">favorite</FontIcon>;
 const nearbyIcon = <IconLocationOn />;
 
+import styles from './styles.css';
+
 export class BottomNavBar extends React.Component { // eslint-disable-line react/prefer-stateless-function
   state = {
     selectedIndex: 0,
@@ -25,25 +27,27 @@ export class BottomNavBar extends React.Component { // eslint-disable-line react
 
   render() {
     return (
-      <Paper zDepth={1}>
-        <BottomNavigation selectedIndex={this.state.selectedIndex}>
-          <BottomNavigationItem
-            label="Recents"
-            icon={recentsIcon}
-            onTouchTap={() => this.select(0)}
-          />
-          <BottomNavigationItem
-            label="Favorites"
-            icon={favoritesIcon}
-            onTouchTap={() => this.select(1)}
-          />
-          <BottomNavigationItem
-            label="Nearby"
-            icon={nearbyIcon}
-            onTouchTap={() => this.select(2)}
-          />
-        </BottomNavigation>
-      </Paper>
+      <div className={styles.footer}>
+        <Paper zDepth={1}>
+          <BottomNavigation selectedIndex={this.state.selectedIndex}>
+            <BottomNavigationItem
+              label="Recents"
+              icon={recentsIcon}
+              onTouchTap={() => this.select(0)}
+            />
+            <BottomNavigationItem
+              label="Favorites"
+              icon={favoritesIcon}
+              onTouchTap={() => this.select(1)}
+            />
+            <BottomNavigationItem
+              label="Nearby"
+              icon={nearbyIcon}
+              onTouchTap={() => this.select(2)}
+            />
+          </BottomNavigation>
+        </Paper>
+      </div>
     );
   }
 }
