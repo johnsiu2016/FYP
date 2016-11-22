393e0c15f4ec2822eb4c92c8c7b939e27cab3c86
nwjohnsiu <john.siu@networld.hk>
2016-11-22 18:46:57 +0800

    add vital sign type handle

--- app/components/VitalSign/index.js
+++ app/components/VitalSign/index.js
@@ -21,44 +21,140 @@ var color = {
   'blue': '#03FDFB',
 };
 
+var data = {
+  'HR': {
+    'top': 120,
+    'bottom': 50,
+    'data': 60
+  },
+  'SpO2': {
+    'top': 100,
+    'bottom': 90,
+    'data': 97
+  },
+  'RP': {
+    'top': 45,
+    'bottom': 8,
+    'data': 40
+  },
+  'ABP': {
+    'systolic': 116,
+    'diastolic': 78,
+    'mean': 91
+  },
+  'PAP': {
+    'systolic': 28,
+    'diastolic': 15,
+    'mean': 21
+  },
+  'NBP': {
+    'systolic': 120,
+    'diastolic': 80,
+    'mean': 90
+  }
+};
+
 class VitalSign extends React.Component { // eslint-disable-line react/prefer-stateless-function
   componentDidUpdate() {
-      global.dispatchEvent(new Event('resize'));
+    global.dispatchEvent(new Event('resize'));
   }
 
   render() {
-    var {containerHeight, w, strokeStyle} = this.props;
-    var scaleRatio = w/12;
+    var {containerHeight, w, strokeStyle, vitalSign} = this.props;
+    var scaleRatio = w / 12;
     var scaleContainerHeight = containerHeight * scaleRatio;
+    var element = null;
 
-    return (
-      <div style={{
-        width: '100%',
-        height: '100%',
-        display: 'flex',
-        flexFlow: 'row wrap',
-        color: color[strokeStyle]
-      }}>
+    switch (vitalSign) {
+      case "HR":
+      case "SpO2":
+      case "RP":
+        element = (
+          <div style={{
+            width: '100%',
+            height: '100%',
+            display: 'flex',
+            flexFlow: 'row wrap',
+            color: color[strokeStyle]
+          }}>
 
-        <div style={{flex: '0.4'}}>
-          <div style={{fontSize: '2em'}}>
-            HR
-          </div>
-          <div style={{alignSelf: 'center'}}>
-            <div style={{fontSize: `${scaleContainerHeight*0.2}px`, lineHeight: `${scaleContainerHeight*0.2}px`, textAlign: 'center'}}>
-              120
+            <div style={{flex: '0.4'}}>
+              <div style={{fontSize: '2em'}}>
+                {vitalSign}
+              </div>
+              <div style={{alignSelf: 'center'}}>
+                <div style={{
+                  fontSize: `${scaleContainerHeight * 0.2}px`,
+                  lineHeight: `${scaleContainerHeight * 0.2}px`,
+                  textAlign: 'center'
+                }}>
+                  {data[vitalSign].top}
+                </div>
+                <div style={{
+                  fontSize: `${scaleContainerHeight * 0.2}px`,
+                  lineHeight: `${scaleContainerHeight * 0.2}px`,
+                  textAlign: 'center'
+                }}>
+                  {data[vitalSign].bottom}
+                </div>
+              </div>
             </div>
-            <div style={{fontSize: `${scaleContainerHeight*0.2}px`, lineHeight: `${scaleContainerHeight*0.2}px`, textAlign: 'center'}}>
-              50
+
+            <div style={{
+              flex: '0.6',
+              fontSize: `${scaleContainerHeight * 0.9}px`,
+              lineHeight: `${scaleContainerHeight * 0.9}px`
+            }}>
+              {data[vitalSign].data}
             </div>
           </div>
-        </div>
+        );
+        break;
+      case "ABP":
+      case "PAP":
+      case "NBP":
+        element = (
+          <div style={{
+            width: '100%',
+            height: '100%',
+            display: 'flex',
+            flexFlow: 'row wrap',
+            color: color[strokeStyle]
+          }}>
 
-        <div style={{flex: '0.6', fontSize: `${scaleContainerHeight*0.9}px`, lineHeight: `${scaleContainerHeight*0.9}px`}}>
-          90
-        </div>
-      </div>
-    );
+            <div style={{flex: '0.15'}}>
+              <div style={{fontSize: '2em'}}>
+                {vitalSign}
+              </div>
+            </div>
+
+            <div style={{
+              flex: '0.6',
+              fontSize: `${scaleContainerHeight * 0.4}px`,
+              lineHeight: `${scaleContainerHeight * 0.4}px`,
+              alignSelf: 'center'
+            }}>
+              {data[vitalSign].systolic}
+              {"/"}
+              {data[vitalSign].diastolic}
+            </div>
+
+            <div style={{
+              flex: '0.25',
+              fontSize: `${scaleContainerHeight * 0.4}px`,
+              lineHeight: `${scaleContainerHeight * 0.4}px`,
+              alignSelf: 'center'
+            }}>
+              {"("}
+              {data[vitalSign].mean}
+              {")"}
+            </div>
+          </div>
+        );
+        break;
+    }
+
+    return element;
   }
 }
 
