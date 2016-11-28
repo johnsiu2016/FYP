0e6c9da77eb7790eb5e7251a5226dea2bcea36cc
johnsiu <buckyscode@gmail.com>
2016-11-28 20:54:27 +0800

    add api route in server

---
+++ server/api/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var mongoose = require('mongoose');
+
+/**
+ * GET /api
+ * List of API examples.
+ */
+exports.getApi = (req, res) => {
+  res.send("OK")
+};
--- server/index.js
+++ server/index.js
@@ -1,21 +1,55 @@
 /* eslint consistent-return:0 */
+/**
+ * Module dependencies.
+ */
 
 const express = require('express');
-const logger = require('./logger');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const path = require('path');
+const mongoose = require('mongoose');
 
+const logger = require('./logger');
 const argv = require('minimist')(process.argv.slice(2));
 const setup = require('./middlewares/frontendMiddleware');
 const isDev = process.env.NODE_ENV !== 'production';
 const ngrok = (isDev && process.env.ENABLE_TUNNEL) || argv.tunnel ? require('ngrok') : false;
-const resolve = require('path').resolve;
 const app = express();
 
-// If you need a backend, e.g. an API, add your custom backend-specific middleware here
-// app.use('/api', myApi);
+/**
+ * Load environment variables from .env file, where API keys and passwords are configured.
+ */
+dotenv.load({ path: 'server/.env' });
+
+/**
+ * Connect to MongoDB.
+ */
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI);
+mongoose.connection.on('error', () => {
+  console.log('%s MongoDB connection error. Please make sure MongoDB is running.', '✗');
+  process.exit();
+});
+
+/**
+ * Express configuration.
+ */
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/**
+ * Route handlers.
+ */
+const apiHandlers = require('./api');
+
+/**
+ * API examples routes.
+ */
+app.get('/api', apiHandlers.getApi);
 
 // In production we need to pass these values in instead of relying on webpack
 setup(app, {
-  outputPath: resolve(process.cwd(), 'build'),
+  outputPath: path.resolve(process.cwd(), 'build'),
   publicPath: '/',
 });
 
